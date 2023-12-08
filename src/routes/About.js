071cdae700018539dded32a3c21e65710923a484
Sushil008 <sushilbaral887@gmail.com>
2023-12-08 16:07:44 +0545

    about page updated

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,23 +1,20 @@
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
-
+import AboutImg from "../assets/machine.jpg"
 function About(){
     return(
         <>
         <Navbar/>
         <Hero
-        cName="hero"
-        heroImg="https://images.unsplash.com/photo-1615906655593-ad0386982a0f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        title="Drive with confidence, repair with excellence – your car's journey in good hands"
-        text="Drive assured, repair with us"
-        buttonText="Your Automobile, Our responsibility"
-        url="/"
-        btnClass="show"
+        cName="hero-mid"
+        heroImg={AboutImg}
+        title="About us"
+        btnClass="hide"
         
-        
+    
         />
         </>
-    )
+    );
 
 
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,7 +10,7 @@ function Home() {
         heroImg="https://images.unsplash.com/photo-1615906655593-ad0386982a0f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         title="Drive with confidence, repair with excellence – your car's journey in good hands"
         text="Drive assured, repair with us"
-        buttonText="Your Automobile, Our responsibility"
+        buttonText="Contact Us"
         url="/"
         btnClass="show"
         
